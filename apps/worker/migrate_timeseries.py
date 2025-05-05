2f8e7cf91690d76f54e7ee249d143fa495c559b8
joseph-sentry <136376984+joseph-sentry@users.noreply.github.com>
2025-05-05 10:18:44 -0400

    fix: change the TA timescale refresh policy (#54)
    * chore: run ruff check
    
    * build: pause ts background worker when running migrations
    
    * fix: change the TA timescale refresh policy
    
    Previously the refresh policy for the TA continuous aggregates had an
    end date of interval 1 day which I thought meant that it would refresh
    all data but the latest (current) time bucket. However, since it would
    refresh all data within this interval:
    
    start_date < date < end_date
    
    and it would go by the time buckets this meant that the current bucket
    AND the previous bucket would be ignored. This is because the date of
    the rows in the previous bucket was == end_date and the range above
    excludes end_date.
    
    So to make sure the previous bucket is included we need to make it so
    end_date = current_date so only today's bucket is ignored. I thought
    setting end_date = null would make this work because the docs mention
    ignoring the latest bucket by default however i couldn't get that to
    work locally so I went with making the end_date's time bucket resolve
    to current date by setting the interval to be very short.
    
    To be extra clear let me illustrate with an example. Today is
    2025-04-25. Any data today will be put in the time bucket 2025-04-25,
    yesterday (today - interval 1 day) will be 2025-04-24, etc. With the
    old policy, today's refresh would look for any changed rows with:
    2025-04-18 < time bucket < 2025-04-24
    which means that any rows from yesterday with time bucket = 2025-04-24
    would be ignored, meaning on 2025-04-25, we're only seeing up to
    2025-04-23's data.
    
    By setting the end date interval to 1 second, the end date becomes
    2025-04-25 and 2025-04-24's data becomes included.

--- apps/worker/migrate_timeseries.py
+++ apps/worker/migrate_timeseries.py
@@ -3,6 +3,7 @@ import os
 
 import django
 from django.core.management import call_command
+from django.db import connections
 
 # Setup Django environment
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_scaffold.settings")
@@ -17,6 +18,10 @@ def run_migrate_commands():
     try:
         if settings.TA_TIMESERIES_ENABLED:
             logger.info("Running ta_timeseries migrations")
+            with connections["ta_timeseries"].cursor() as cursor:
+                cursor.execute(
+                    "SELECT _timescaledb_internal.stop_background_workers();"
+                )
             call_command(
                 "migrate",
                 database="ta_timeseries",
@@ -24,6 +29,10 @@ def run_migrate_commands():
                 settings="django_scaffold.settings",
                 verbosity=1,
             )
+            with connections["ta_timeseries"].cursor() as cursor:
+                cursor.execute(
+                    "SELECT _timescaledb_internal.start_background_workers();"
+                )
         else:
             logger.info("Skipping ta_timeseries migrations")
 
